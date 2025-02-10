2705454aeda543b4736f4e81bcf151c28d22acdc
219WD <macgunmanmc@gmail.com>
2025-02-10 15:20:01 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,30 @@
+export const stories = [
+  {
+    profileImg: "./assets/profile-2.png",
+    profileName: "Instagram",
+    title: [
+      "Kaninofunk",
+      "El Rapero del Tren Roca",
+      "Seguime en Instagram",
+    ],
+    linkLabel: "Pagina de Instagram",
+    linkSrc: "https://www.instagram.com/kaninofunk",
+    storyImg: "./assets/story-1.png",
+  },
+  {
+    profileImg: "./assets/profile-4.png",
+    profileName: "Tik Tok",
+    title: ["Seguime en Tik Tok", "Mirá mis últimos videos", "A puro flow."],
+    linkLabel: "Canal de Tik Tok",
+    linkSrc: "https://www.tiktok.com/@elraperodeltrenroca",
+    storyImg: "./assets/story-2.png",
+  },
+  {
+    profileImg: "./assets/profile-5.png",
+    profileName: "Youtube",
+    title: ["Videoclips oficiales", "Lanzamientos", "Y mucho más."],
+    linkLabel: "Canal de Youtube",
+    linkSrc: "https://api.whatsapp.com/send/?phone=5493816671884&text=Hola,%20quiero%20contratar%20tus%20Web%20Services.&type=phone_number&app_absent=0",
+    storyImg: "./assets/story-3.png",
+  },
+];
